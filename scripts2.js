55c254440cad3a5610ca74e746a1a7d6ae2f4744
Matheus-1213 <86915325+Matheus-1213@users.noreply.github.com>
2021-07-04 10:11:18 -0300

    Add files via upload

---
+++ scripts2.js
@@ -0,0 +1,14 @@
+let isConfirmed = confirm("deseja continuar?")
+
+console.log(isConfirmed)
+
+if (isConfirmed) {
+    console.log("o usuário clicou em Ok")
+}
+else{
+    console.log("o usuário clicou em Cancelar")
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
